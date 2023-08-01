ea4b08eb5a6ad2882713ea68ad99eacebabd041c
JuliZpta <141162903+JuliZpta@users.noreply.github.com>
2023-08-01 11:30:32 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+let player = "Julian";
+player = 2023;
+
+let playerDos = "Peludito";
+
+console.log(playerDos);
+
+let nombre = "Juli";
+console.log("Hola Mundo " + nombre);
+
+console.log(`Mi nombre es: ${nombre}`);
